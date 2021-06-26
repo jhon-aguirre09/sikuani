e52a3f283515c31248a8fbd9a35a9b1441fa0f27
Jon-MC-dev <66422508+Jon-MC-dev@users.noreply.github.com>
2021-06-26 09:43:14 -0500

    Sockets implementados

--- app_star.py
+++ app_star.py
@@ -1,10 +1,27 @@
 from flask import Flask
+from flask_socketio import SocketIO, send
 import pymongo
 
+
 app = Flask(__name__)
+socket_io = SocketIO(app)
 
 @app.route('/')
 def hello():
     client = pymongo.MongoClient("mongodb+srv://new_user:sikuani@cluster0.vxniy.mongodb.net/sikuanidb?retryWrites=true&w=majority") 
     db = client.test
     return str(db)
+
+# Suceptible a cambios
+#@app.route('/')
+#def index():
+#    return render_template('index.html')
+
+
+@socket_io.on('message')
+def escucha(numero):
+    print('Numero: ', numero)
+    valor_random = randint(10, 100)
+    if valor_random == numero:
+        valor_random = randint(10, 100)
+    send(str(valor_random), broadcast = True)
