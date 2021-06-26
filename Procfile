38b8faa076c769f4fd4919edb1c781ad4bf27e40
jhon-aguirre09 <jhon.aguirre@ssc.com.co>
2021-06-25 21:17:08 -0500

    Deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn sgi_station46.wsgi --log-file -
---
+++ requirements.py
@@ -0,0 +1,12 @@
+certifi==2021.5.30
+click == 8.0.1
+colorama==0.4.4
+Flask == 8.0.1
+gunicorn==20.1.0
+importlib-metadata == 3.10.0
+itsdangerous == 2.0.1
+Jinja2 == 3.0.0
+MarkupSafe == 2.0.1
+Werkzeug == 1.0.1
+wincertstore==0.2
+zipp == 3.4.1
