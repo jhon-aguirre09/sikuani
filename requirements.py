7cf9377a5aa47c97ce674be416fae9f2022020b6
jhon-aguirre09 <jhon.aguirre@ssc.com.co>
2021-06-25 22:04:19 -0500

    Implementacion Mongodb

--- requirements.py
+++ requirements.py
@@ -1,12 +1,16 @@
 certifi==2021.5.30
 click == 8.0.1
 colorama==0.4.4
+dnspython          2.1.0
 Flask == 8.0.1
 gunicorn==20.1.0
 importlib-metadata == 3.10.0
 itsdangerous == 2.0.1
 Jinja2 == 3.0.0
 MarkupSafe == 2.0.1
+pymongo == 3.11.4
+setuptools == 52.0.0.post20210125
+wheel == 0.36.2
 Werkzeug == 1.0.1
 wincertstore==0.2
 zipp == 3.4.1
