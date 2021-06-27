2beeacd238cc0713371e75a9942ab7f5e0fccc3c
jhon-aguirre09 <jhon.aguirre@ssc.com.co>
2021-06-27 12:54:22 -0500

    Conexion servidor

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, request
+from clases.peticiones import PeticionDatos
 import pymongo
+
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello():
-    client = pymongo.MongoClient("mongodb+srv://new_user:sikuani@cluster0.vxniy.mongodb.net/sikuanidb?retryWrites=true&w=majority") 
-    db = client.test
-    return str(db)
+
+    peticion = PeticionDatos(2)
+    
+    if request.method == 'GET':
+        client = pymongo.MongoClient("mongodb+srv://new_user:sikuani@cluster0.vxniy.mongodb.net/sikuanidb?retryWrites=true&w=majority") 
+        db = client.test
+        return str(db)
+        
+    if request.method == 'POST':
+        request_data = request.get_json()
+        return request_data
