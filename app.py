b5966093c3f5c47af66e8dbfa896173361b7c2b4
jhon-aguirre09 <jhon.aguirre@ssc.com.co>
2021-06-27 13:27:34 -0500

    app

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ def hello():
         return str(db)
         
     if request.method == 'POST':
-        # request_data = request.get_json()
-        return "Hecho"
+        request_data = request.json()
+        return "<h1>Bienvenido " + request_data + "</h1>"
 
     # def show_data():
     #     return peticion
